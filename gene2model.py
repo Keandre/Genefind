2f00e6460929e0653c8ce952c7e40a5262a375df
Vincent Wing <vincentwing00@gmail.com>
2019-07-18 21:05:42 -0700

    fixed issues with formatting and spacing, but atom positions seem wrong.

--- gene2model.py
+++ gene2model.py
@@ -33,17 +33,17 @@ class Gene:
             xmin = min(xmin, acid.min_x())
             ymin = min(ymin, acid.min_y())
             zmin = min(zmin, acid.min_z())
-        box_size_x = str((2 * (xmax-xmin)))
-        box_size_y = str((2 * (ymax-ymin)))
-        box_size_z = str((2 * (zmax-zmin)))
+        box_size_x = "{:.3f}".format((2 * (xmax-xmin)))
+        box_size_y = "{:.3f}".format((2 * (ymax-ymin)))
+        box_size_z = "{:.3f}".format((2 * (zmax-zmin)))
 
         # Get column lengths so that we can format it in fixed width format
         column_lengths = [5 + len(str(len(self._acids)))] #first column: acid number and name
         column_lengths.append(7) #second column: atom name
-        column_lengths.append(len(str(num_atoms))) #third column: atom sequence number
-        column_lengths.append((3 + len(str(max(abs(xmax), abs(xmin)))))) #fourth column: atom x coordinate
-        column_lengths.append((3 + len(str(max(abs(ymax), abs(ymin)))))) #fifth column: atom y coordinate
-        column_lengths.append((3 + len(str(max(abs(zmax), abs(zmin)))))) #sixth column: atom z coordinate
+        column_lengths.append((1 + len(str(num_atoms)))) #third column: atom sequence number
+        column_lengths.append((3 + len("{:.3f}".format(max(abs(xmax), abs(xmin)))))) #fourth column: atom x coordinate
+        column_lengths.append((3 + len("{:.3f}".format(max(abs(ymax), abs(ymin)))))) #fifth column: atom y coordinate
+        column_lengths.append((3 + len("{:.3f}".format(max(abs(zmax), abs(zmin)))))) #sixth column: atom z coordinate
 
         #add title to .gro string
         gromacs = title + "\n "
@@ -124,42 +124,47 @@ class AminoAcid:
     def convert_to_first_acid(self, beginning_atoms):
         """First acid in protein chain has a different amino group structure."""
         beginning_atoms.change_name(self.name)
+        beginning_atoms.accomodate_atom(self.head())
         self._atoms[1] = beginning_atoms.atom_at_index(0)
         self._atoms.insert(2, beginning_atoms.atom_at_index(1))
         self._atoms.insert(3, beginning_atoms.atom_at_index(2))
-        self._atoms[1].update_pos(self.head())
-        self._atoms[2].update_pos(self.head())
-        self._atoms[3].update_pos(self.head())
-
+        
     def convert_to_last_acid(self, ending_atoms):
         """Last acid in protein chain has a different carboxyl group structure."""
         ending_atoms.change_name(self.name)
+        ending_atoms.accomodate_atom(self.atom_at_index(-3))
         self._atoms[-1] = ending_atoms.atom_at_index(-2)
         self._atoms.append(ending_atoms.atom_at_index(-1))
-        self._atoms[-2].update_pos(self.head())
-        self._atoms[-1].update_pos(self.head())
-    
+
+    def accomodate_atom(self, atom):
+        """Moves atoms in the acid so that it appears next to an atom."""
+        for atom in self._atoms:
+            atom.update_pos(atom)
+
     def accomodate(self, acid):
         """Moves atoms in the acid so that it appears at the end of the chain."""
-        for atom in self._atoms:
-            atom.update_pos(acid.tail())
+        self.accomodate_atom(acid.tail())
 
     def to_gromacs(self, column_lengths, acid_num, atom_num):
         """Convert the acid to a series of gromacs text format lines."""
         gromacs = ""
         for atom in self._atoms:
+            coords = atom.get_coordinates()
+            xcoord = "{:.3f}".format(coords[0])
+            ycoord = "{:.3f}".format(coords[1])
+            zcoord = "{:.3f}".format(coords[2])
             gromacs += ' ' * (column_lengths[0] - len(self.name) - len(str(acid_num)))
             gromacs += str(acid_num) + self.name #residue name and number
             gromacs += ' ' * (column_lengths[1] - len(atom.get_name()))
             gromacs += atom.get_name() #atom name
             gromacs += ' ' * (column_lengths[2] - len(str(atom_num)))
             gromacs += str(atom_num) #atom number
-            gromacs += ' ' * (column_lengths[3] - len(str(atom.get_coordinates()[0])))
-            gromacs += str(atom.get_coordinates()[0]) #x coordinate
-            gromacs += ' ' * (column_lengths[4] - len(str(atom.get_coordinates()[1])))
-            gromacs += str(atom.get_coordinates()[1]) #y coordinate
-            gromacs += ' ' * (column_lengths[5] - len(str(atom.get_coordinates()[2])))
-            gromacs += str(atom.get_coordinates()[2]) + '\n' #z coordinate
+            gromacs += ' ' * (column_lengths[3] - len(xcoord))
+            gromacs += xcoord #x coordinate
+            gromacs += ' ' * (column_lengths[4] - len(ycoord))
+            gromacs += ycoord #y coordinate
+            gromacs += ' ' * (column_lengths[5] - len(zcoord))
+            gromacs += zcoord + '\n' #z coordinate
             atom_num += 1
         return gromacs
             
@@ -251,7 +256,31 @@ class GeneToModelConverter:
             acid.push_atom(atom)
         return acid
 
-    def gene_convert(self, codons, filepath):
+    def convert_acids_to_gene(self, acid_names, filepath):
+        """Convert a given acid string to a .gro position model."""
+        acids_list = [acid_names[i:i+3] for i in range(0, len(acid_names), 3)]
+        acids = []
+        for acid_name in acids_list:
+            acids.append(self.acid_table[acid_name])
+        end_index = -1
+        for i in range(0, len(acids)):
+            if acids[i].get_name() == "END":
+                end_index = i
+                break
+        if end_index >= 0:
+            del acids[end_index:len(acids)]
+
+        acids[0].convert_to_first_acid(self.acid_table['BEG'])
+        acids[-1].convert_to_last_acid(self.acid_table['END'])
+
+        gene = Gene(acids)
+        path = Path(filepath)
+        gromacs = gene.to_gromacs(path.stem)
+        file = open(filepath, 'w')
+        file.write(gromacs)
+        file.close()
+
+    def convert_codons_to_gene(self, codons, filepath):
         """Convert a given codon string to a .gro position model."""
         codon_to_acid = {
             "AAA":"LYS", "AAT":"ASN", "AAG":"LYS",
@@ -278,25 +307,9 @@ class GeneToModelConverter:
             "CCC":"PRO"}
         
         codons_list = [codons[i:i+3] for i in range(0, len(codons), 3)]
-        acids = [self.acid_table[codon_to_acid[codons_list[i]]] for i in range(0, len(codons_list))]
-
-        end_index = -1
-        for i in range(0, len(acids)):
-            if acids[i].get_name() == "END":
-                end_index = i
-                break
-        if end_index >= 0:
-            del acids[end_index:len(acids)]
-
-        acids[0].convert_to_first_acid(self.acid_table['BEG'])
-        acids[-1].convert_to_last_acid(self.acid_table['END'])
-
-        gene = Gene(acids)
-        path = Path(filepath)
-        gromacs = gene.to_gromacs(path.stem)
-        file = open(filepath, 'w')
-        file.write(gromacs)
-        file.close()
+        acid_names_list = [codon_to_acid[codon] for codon in codons_list]
+        acid_names = ''.join(acid_names_list)
+        self.convert_acids_to_gene(acid_names, filepath)
     
 # old pdb methods for making acid table
 
@@ -314,8 +327,7 @@ class GeneToModelConverter:
                                          'x', 'y', 'z',
                                          'occupancy', 'temp_factor', 'charge'])
         model = False # Whether we're currently reading lines from a model
-        previous_tail = Atom('origin', AminoAcid('placeholder'),
-                             [0,0,0])
+        previous_tail = Atom('origin', AminoAcid('placeholder'), [0,0,0])
         acid_ranges = []
         current_range = []
         for line in enumerate(lines):
@@ -361,4 +373,5 @@ class GeneToModelConverter:
 # Testing code
 
 table = GeneToModelConverter("amino_acid_reference.txt")
-table.gene_convert("AAAGTCCGG", "test1.gro")
+table.convert_codons_to_gene("AAAGCCATG", "test1.gro")
+table.convert_acids_to_gene("LYSVALPHEGLYARGCYSGLULEUALAALAALAMETLYSARGHISGLYLEUASPASNTYRARGGLYTYRSERLEUGLYASNTRPVALCYSALAALALYSPHEGLUSERASNPHEASNTHRGLNALATHRASNARGASNTHRASPGLYSERTHRASPTYRGLYILELEUGLNILEASNSERARGTRPTRPCYSASNASPGLYARGTHRPROGLYSERARGASNLEUCYSASNILEPROCYSSERALALEULEUSERSERASPILETHRALASERVALASNCYSALALYSLYSILEVALSERASPGLYASNGLYMETASNALATRPVALALATRPARGASNARGCYSLYSGLYTHRASPVALGLNALATRPILEARGGLYCYSARGLEU", "LYSOZYME.gro")
